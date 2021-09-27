08264100a1e69483298e686b4dec2f059233ab40
joscamphy <joseph.marcel.a@gmail.com>
2021-09-27 21:19:03 +0200

    with mock server details

--- webapp/test/initMockServer.js
+++ webapp/test/initMockServer.js
@@ -1,18 +1,11 @@
 sap.ui.define([
-	"../localService/mockserver",
-	"sap/m/MessageBox"
-], function (mockserver, MessageBox) {
+	"../localservice/mockserver"
+], function (mockserver) {
 	"use strict";
 
-	var aMockservers = [];
-
 	// initialize the mock server
-	aMockservers.push(mockserver.init());
+	mockserver.init();
 
-	Promise.all(aMockservers).catch(function (oError) {
-		MessageBox.error(oError.message);
-	}).finally(function () {
-		// initialize the embedded component on the HTML page
-		sap.ui.require(["sap/ui/core/ComponentSupport"]);
-	});
+	// initialize the embedded component on the HTML page
+	sap.ui.require(["sap/ui/core/ComponentSupport"]);
 });
